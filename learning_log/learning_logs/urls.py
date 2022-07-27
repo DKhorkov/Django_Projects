191f3c1d9a8c2846ee39404b3358e04aa85f13f5
DKhorkov <dkhorkov.dev@gmail.com>
2022-07-27 21:12:46 +0300

    Added a function to edit entries

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('topics/<topic_id>', views.topic, name='topic'),  # Страница с конкретной темой, выбранной пользователем
     path('new_topic', views.new_topic, name='new_topic'),  # Страница для добавления новой темы
     path('new_entry/<topic_id>', views.new_entry, name='new_entry'),  # Страница для добавления новой записи по теме
+    path('edit_entry/<entry_id>', views.edit_entry, name='edit_entry'),  # Страница редактирования записи
 ]
