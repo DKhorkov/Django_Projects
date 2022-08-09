06d5f1375c2de198d1a9581f99a9ed6b537e2088
DKhorkov <dkhorkov.dev@gmail.com>
2022-08-09 22:49:13 +0300

    Create an option for user to change password

--- natalia_toys/users/urls.py
+++ natalia_toys/users/urls.py
@@ -6,4 +6,7 @@ app_name = 'users'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('profile', views.profile, name='profile'),
+    path('profile_changed', views.profile_changed, name='profile_changed'),
+    path('password_changed', views.password_changed, name='password_changed'),
 ]
--- natalia_toys/users/views.py
+++ natalia_toys/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.forms import PasswordChangeForm
+
+
 from .forms import UserCreationForm
-# Create your views here.
 
 
 def register(request):
@@ -17,3 +19,30 @@ def register(request):
 
     context = {'form': form}
     return render(request, 'registration/register.html', context)
+
+
+def profile_changed(request):
+    """Переадресация на страницу с инфой об успешном изменении профиля"""
+    return render(request, 'registration/profile_changed.html')
+
+
+def password_changed(request):
+    """Переадресация на страницу с инфой об успешном изменении пароля"""
+    return render(request, 'registration/password_changed.html')
+
+
+def profile(request):
+    """Профиль пользователя, где он может редактировать свои данные."""
+    user = request.user
+
+    if request.method != 'POST':
+        form = PasswordChangeForm(user)
+    else:
+        form = PasswordChangeForm(user, data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect('users:password_changed')
+
+    context = {'form': form}
+    return render(request, 'registration/profile.html', context)
